5374b9c8953b80a2975813b06a4914a82ef43366
Rishats <sultanov-rishat@inbox.ru>
2017-04-24 20:40:02 +0600

    Convert in to Object oriented programming style.

--- vendor/index.php
+++ vendor/index.php
@@ -4,14 +4,21 @@ include "autoload.php";
 use DiDom\Document;
 
 
-$document = new Document('https://pogoda.mail.ru/prognoz/bishkek/', true);
+class Main
+{
+    public function speedWind()
+    {
+        $document = new Document('https://pogoda.mail.ru/prognoz/bishkek/', true);
+        $posts = $document->find('.information__content__additional__item');
+        $str = strval($posts[3]);
+        $speed = explode(" ",$str);
+        $speed = $speed[3];
+        return $speed;
+    }
+}
 
 
-$posts = $document->find('.information__content__additional__item');
-$str = strval($posts[3]);
-$speed = explode(" ",$str);
-$speed = $speed[3];
+$speed = new Main();
 
-
-echo $speed;
+echo $speed->speedWind();
 
