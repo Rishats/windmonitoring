f37532c8bdd052ffd5350fbf03b7b1ca3d38cb11
Rishatos <sultanov-rishat@inbox.ru>
2017-05-15 18:56:04 +0600

    JSON encode - Speed 24 hours

---
+++ parsing/logbishkek24.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'connection.php'; // подключаем скрипт с коннектом
+// подключаемся к серверу ( БД )
+$link = mysqli_connect($host, $user, $password, $database)
+or die("Ошибка подключения к базе :) " . mysqli_error($link));
+
+// выполняем операции с базой данных
+$query ="SELECT * FROM windspeed WHERE time >= now() - INTERVAL 1 DAY;";
+$result = mysqli_query($link, $query) or die("Ошибка =======   " . mysqli_error($link));
+// закрываем подключение
+mysqli_close($link);
+$arrays = array();
+$arraydone = array();
+foreach ($result as $element)
+{
+    array_push($arrays,$element);
+}
+foreach ( $arrays as $array ) {
+
+
+    foreach ( $array as $key => $value ) {
+        if ($key == 'speed')
+        {
+            array_push($arraydone,$value);
+        }
+    }
+
+    echo '</dl>';
+}
+echo json_encode($arraydone);
+?>
